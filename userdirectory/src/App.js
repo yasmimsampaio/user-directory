77da766d4b527f8871cd88a9605cccda5c78767e
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2021-01-20 20:54:08 -0500

    refactor

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -6,19 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Party <code>src/App.js</code> and save to reload.
-        </p>
         <Table/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
